ac586f12483bb6935d7547acd3eeea90c179040c
leventkalkavan <levent.kalkavan@hotmail.com>
2024-03-28 01:12:28 +0300

    One to Many Relation

--- smartedu_con/courses/views.py
+++ smartedu_con/courses/views.py
@@ -1,21 +1,55 @@
 from django.shortcuts import render
-from . models import Course
+from . models import Course, Category, Tag
 
 def course_list(request):
     courses = Course.objects.all().order_by('-date')
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
 
     context = {
-        'courses': courses
+        'courses': courses,
+        'categories': categories,
+        'tags': tags
+
     }
 
     return render(request,"courses.html", context)
 
 def course_detail(request,category_slug,course_id):
-    course = Course.objects.get(category__slug = category_slug, id= course_id)
+    course = Course.objects.get(category__slug = category_slug,id= course_id)
+    categories = Category.objects.all()
 
     context = {
-        'course': course
+        'course': course,
+        'categories': categories
     }
 
     return render(request,"course.html", context)
+
+def category_list(request,category_slug):
+    courses = Course.objects.filter(category__slug = category_slug)
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+    context = {
+        'courses': courses,
+        'categories': categories,
+        'tags': tags
+    }
+
+    return render(request,"courses.html", context )
+
+
+def tag_list(request, tag_slug):
+    courses = Course.objects.all().filter(tags__slug=tag_slug)
+    categories = Category.objects.all()
+    tags= Tag.objects.all()
+
+    context = {
+        'courses': courses,
+        'categories': categories,
+        'tags':tags
+    }
+
+    return render(request, 'courses.html', context)
 
